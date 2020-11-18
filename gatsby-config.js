d2edf9db0c5bda15e8255e531f97ed5e18026f63
marialai <74628288+marialai@users.noreply.github.com>
2020-11-17 16:28:30 -0800

    Update gatsby-config.js

--- gatsby-config.js
+++ gatsby-config.js
@@ -7,7 +7,7 @@ module.exports = {
     // Main Site Title
     title: `Maria Lai | Product Manager`,
     // Description that goes under your name in main bio
-    description: `Lorem ipsum dolor sit amet consectetur adipisicing elit, ipsum.`,
+    description: `Product Manager @ Snap`,
     // Optional: Twitter account handle
     author: `@marialaitt`,
     // Optional: Github account URL
@@ -17,6 +17,19 @@ module.exports = {
     // Content of the About Me section
     about: `Lorem ipsum dolor sit amet consectetur adipisicing elit. Ducimus perferendis porro cumque ea error ab voluptatem. Temporibus adipisci exercitationem similique itaque quibusdam laudantium, qui molestiae quas, aut amet animi id.`,
     // Optional: List your projects, they must have `name` and `description`. `link` is optional.
+        // Optional: List your experience, they must have `name` and `description`. `link` is optional.
+    experience: [
+      {
+        name: 'Snap',
+        description: 'Product Manager for Play Canvas',
+        link: 'https://snapchat.com/',
+      },
+      {
+        name: 'Microsoft',
+        description: 'Product Manager II for Azure AD',
+        link: 'https://azure.microsoft.com/services/active-directory/',
+      },
+    ],
     projects: [
       {
         name: 'Devfolio',
@@ -37,24 +50,7 @@ module.exports = {
         link: 'https://github.com/RyanFitzgerald/devfolio',
       },
     ],
-    // Optional: List your experience, they must have `name` and `description`. `link` is optional.
-    experience: [
-      {
-        name: 'Snap',
-        description: 'Product Manager for Play Canvas',
-        link: 'https://github.com/RyanFitzgerald/devfolio',
-      },
-      {
-        name: 'Microsoft',
-        description: 'Product Manager II for Azure AD',
-        link: 'https://github.com/RyanFitzgerald/devfolio',
-      },
-      {
-        name: 'Hooli',
-        description: 'Full-Stack Developer, May 2015 - December 2017',
-        link: 'https://github.com/RyanFitzgerald/devfolio',
-      },
-    ],
+
     // Optional: List your skills, they must have `name` and `description`.
     skills: [
       {
